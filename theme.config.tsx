68e98bdcbf8cefd16e36c5a507168052823736fa
Pavel <pbkrutov@gmail.com>
2024-05-02 22:20:52 +0300

    Initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Everthorn Docs</span>,
+  project: {
+    link: 'https://github.com/protocolpav/nexuscore',
+  },
+  docsRepositoryBase: 'https://github.com/protocolpav/everthorn-docs',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
